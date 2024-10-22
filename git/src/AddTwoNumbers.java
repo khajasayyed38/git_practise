cdecc832dfe51a6b3ceaa2a87b1ef5da27cb0bd8
khajasayyed10 <khajasayyed10@gmail.com>
2024-10-22 13:32:53 +0530

    added sum of number

---
+++ git/src/AddTwoNumbers.java
@@ -0,0 +1,10 @@
+package src;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        System.out.println(add(6,8));
+    }
+    public static int add(int a,int b){
+        return a+b;
+    }
+}
